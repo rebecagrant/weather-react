5463a7ef35f21e9fb6c6a499b41a5ab97de45691
rebecagrant <72933534+rebecagrant@users.noreply.github.com>
2021-01-13 21:27:08 +0000

    Corrections

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import Temperature from "./Temperature";
 
 export default function App() {
   return (
-    <div className="weather-app rounded">
-      <Search />
-      <Location />
-      <Temperature />
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <h1>Weather Forecast</h1>
+      </div>
+      <div className="weather-app rounded">
+        <Search />
+        <Location />
+        <Temperature />
+      </div>
+      <footer class="row justify-content-center">
+        <div>
+          <a href="https://github.com/rebecagrant/" target="_blank">Open-source code</a> by
+          <a href="https://github.com/rebecagrant/" target="_blank">Rebeca Grant</a>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Weather() {
-    return (<div><h3>Hello from  Weather</h3></div>);
-}
