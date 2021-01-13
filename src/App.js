58b15c4cd289cefec5f4ab3c1a833378221c8cea
rebecagrant <72933534+rebecagrant@users.noreply.github.com>
2021-01-13 21:32:50 +0000

    center h1

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Temperature from "./Temperature";
 
 export default function App() {
   return (
-    <div class="container">
-      <div class="row justify-content-center mt-5">
-        <h1>Weather Forecast</h1>
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h1 className="centered">Weather Forecast</h1>
       </div>
       <div className="weather-app rounded">
         <Search />
         <Location />
         <Temperature />
       </div>
-      <footer class="row justify-content-center">
+      <footer className="row justify-content-center">
         <div>
           <a href="https://github.com/rebecagrant/" target="_blank">Open-source code</a> by
           <a href="https://github.com/rebecagrant/" target="_blank">Rebeca Grant</a>
