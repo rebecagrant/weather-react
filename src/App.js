22e1f7346bc2d9699a5e9f6ba2be1cfda208a7a9
rebecagrant <72933534+rebecagrant@users.noreply.github.com>
2021-01-25 13:15:06 +0000

    Added bootstrap to npm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+
 import './App.css';
+
 import Search from "./Search";
 import Location from "./Location";
 import Temperature from "./Temperature";
 
+
+
 export default function App() {
   return (
     <div className="container">
+
       <div className="row justify-content-center mt-5">
         <h1>Weather Forecast</h1>
       </div>
@@ -17,10 +22,10 @@ export default function App() {
       </div>
       <footer className="row justify-content-center">
         <div>
-          <a href="https://github.com/rebecagrant/weather-react" target="_blank" rel="noopener noreferrer">Open-source code</a> by
-          <a href="https://github.com/rebecagrant/" target="_blank" rel="noopener noreferrer"> Rebeca Grant</a>
+          <a href="https://github.com/rebecagrant/weather-react" target="_blank" rel="noopener noreferrer">Open-sourced code</a> by Rebeca Grant
         </div>
       </footer>
+      
     </div>
   );
 }
