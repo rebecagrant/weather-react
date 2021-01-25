a6711fcdfe82b960f811de61ac61149ad8f9c8fc
rebecagrant <72933534+rebecagrant@users.noreply.github.com>
2021-01-25 15:23:20 +0000

    updates

--- src/Search.js
+++ src/Search.js
@@ -3,15 +3,15 @@ import "./Search.css";
 
 export default function Search() {
   return (
-    <div className="row mb-4 justify-content-md-center">
-      <div className="col-6 city">
+    <div className="row mb-4 form-search">
+      <form className="col-6">
         <input
           type="text"
           className="form-control"
           placeholder="Enter a city.."
           autoComplete="off"
-        />
-      </div>
+        />  
+      </form>
       <form className="col-auto">
         <button type="submit" className="btn btn-dark button1">
           <i className="fas fa-search" />
